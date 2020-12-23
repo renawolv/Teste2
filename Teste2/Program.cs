ee2b439828264c50af8cbb7ccc75fc49e1ba7ada
Renan Ferraz <renanferraz.rw@hotmail.com>
2020-12-23 16:48:33 -0300

    primeira versão

---
+++ Teste2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Teste2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numInicial = 1;
+            int numFinal = 100;
+
+            ExibirNumero(numInicial, numFinal);
+
+            Console.ReadLine();
+        }
+
+        public static void ExibirNumero(int numInicial, int numFinal)
+        {
+            if (numInicial <= numFinal)
+            {
+                Console.Write(numInicial + " ");
+                ExibirNumero(numInicial + 1, numFinal);
+            }
+        }
+    }
+}
